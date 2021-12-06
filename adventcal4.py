6737e87602e673541b0b9d083cd2a9674f1a7b3e
sakuragts <sakura_gts@hotmail.com>
2021-12-06 18:11:13 -0500

    Add files via upload

--- adventcal4.py
+++ adventcal4.py
@@ -5,11 +5,18 @@ bingo_list = list(bingo)
 bingo_draw = [num[0] for num in bingo_list[0]]
 bingo_cards = []
 
+
 i = 2
 while i < len(bingo_list):
     bingo_cards.append(bingo_list[i])
     i = i + 1
 
-for i in range(5):
+def position_cards():
+    count_cards = 0
+    index_cards = []
     for row in bingo_cards:
-        print(row)
+        count_cards = count_cards + 1
+        #empty array = new card
+        if not row:
+            index_cards.append(count_cards - 6)
+    return index_cards
